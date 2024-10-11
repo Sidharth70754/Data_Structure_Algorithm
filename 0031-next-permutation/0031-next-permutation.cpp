8f2580fe2b30fe23e557a9a5369889db34e5d95f
Sidharth Mehta <145455338+Sidharth70754@users.noreply.github.com>
2024-10-11 10:41:11 +0530

    Time: 0 ms (100%), Space: 14.6 MB (96.02%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -2,23 +2,31 @@ class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
         int ind = -1;
-        int n= nums.size();
-    for(int i = n-2; i>=0;i--){
-        if(nums[i]<nums[i+1]){
-            ind = i;
-            break;
+        int n = nums.size();
+        
+        // Step 1: Find the first decreasing element from the back
+        for (int i = n - 2; i >= 0; i--) {
+            if (nums[i] < nums[i + 1]) {
+                ind = i;
+                break;
+            }
         }
-    }
-    if(ind == -1){
-        reverse(nums.begin(), nums.end());
-        return;
-    }
-    for(int i = n-1; i>ind;i--){
-        if(nums[i] > nums[ind]){
-            swap(nums[i],nums[ind]);
-            break;
+
+        // Step 2: If no decreasing element is found, reverse the entire array
+        if (ind == -1) {
+            reverse(nums.begin(), nums.end());
+            return;
         }
+
+        // Step 3: Find the next larger element from the end
+        for (int i = n - 1; i > ind; i--) {
+            if (nums[i] > nums[ind]) {
+                swap(nums[i], nums[ind]);
+                break;
+            }
+        }
+
+        // Step 4: Reverse the portion of the array from ind + 1 to the end
+        reverse(nums.begin() + ind + 1, nums.end());
     }
-    reverse(nums.begin() + ind +1, nums.end());
-}
 };
