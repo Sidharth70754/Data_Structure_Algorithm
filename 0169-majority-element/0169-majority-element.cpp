5b92054066c38b9f89dc98ef269116dd2401016a
Sidharth <145455338+Sidharth70754@users.noreply.github.com>
2025-07-11 14:14:18 +0530

    Time: 0 ms (100%), Space: 28.2 MB (26.02%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,8 +1,15 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        int n = nums.size();
-        return nums[n/2];
+        map<int, int> mpp;
+        for (int i = 0; i < nums.size(); i++) {
+            mpp[nums[i]]++;
+        }
+        for (auto it : mpp) {
+            if (it.second > (nums.size() / 2)) {
+                return it.first;
+            }
+        }
+        return -1;
     }
 };
