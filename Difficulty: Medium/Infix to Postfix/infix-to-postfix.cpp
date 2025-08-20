fceb6c1e14c96804a9f5da4eb2bdc7959be88b0b
Sidharth <145455338+Sidharth70754@users.noreply.github.com>
2025-08-20 12:42:54 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Infix to Postfix/infix-to-postfix.cpp
+++ Difficulty: Medium/Infix to Postfix/infix-to-postfix.cpp
@@ -1,82 +1,38 @@
-//{ Driver Code Starts
-// C++ implementation to convert infix expression to postfix
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
 class Solution {
-public:
-    // Function to return precedence of operators
-    int priority(char c) {
+    int prec(char c) {
         if (c == '^') return 3;
-        if (c == '*' || c == '/' || c == '%') return 2;
+        if (c == '*' || c == '/') return 2;
         if (c == '+' || c == '-') return 1;
         return 0;
     }
-
-    // Function to convert an infix expression to a postfix expression.
-    string infixToPostfix(string s) {
+public:
+    string infixToPostfix(string &s) {
+        string ans;
         stack<char> st;
-        string ans = "";
 
-        for (int i = 0; i < s.size(); i++) {
-            char c = s[i];
-            
-            // If operand, add to result
-            if (isalnum(c)) {
-                ans += c;
-            }
-            // If '(', push to stack
-            else if (c == '(') {
-                st.push(c);
-            }
-            // If ')', pop and add to result until '(' is found
-            else if (c == ')') {
+        for (char ch : s) {
+            if (ch == ' ') continue;
+
+            if (isalnum(ch)) {
+                ans += ch;                         // operand → output
+            } else if (ch == '(') {
+                st.push(ch);                       // '(' → push
+            } else if (ch == ')') {
                 while (!st.empty() && st.top() != '(') {
-                    ans += st.top();
-                    st.pop();
+                    ans += st.top(); st.pop();     // pop till '('
                 }
-                st.pop(); // Remove '(' from stack
-            }
-            // Operator
-            else {
-                while (!st.empty() && priority(c) <= priority(st.top())) {
-                    ans += st.top();
-                    st.pop();
+                if (!st.empty()) st.pop();         // pop '('
+            } else {                                // operator
+                // LEFT-associative for '^' as per your judge: pop on >= precedence
+                while (!st.empty() && st.top() != '(' &&
+                       prec(st.top()) >= prec(ch)) {
+                    ans += st.top(); st.pop();
                 }
-                st.push(c);
+                st.push(ch);
             }
         }
-
-        // Pop remaining operators
-        while (!st.empty()) {
-            ans += st.top();
-            st.pop();
-        }
-
+        while (!st.empty()) { ans += st.top(); st.pop(); }
         return ans;
     }
 };
 
-
-//{ Driver Code Starts.
-//  Driver program to test above functions
-int main() {
-    int t;
-    cin >> t;
-    cin.ignore(INT_MAX, '\n');
-    while (t--) {
-        string exp;
-        cin >> exp;
-        Solution ob;
-        cout << ob.infixToPostfix(exp) << endl;
-
-        cout << "~"
-             << "\n";
-    }
-    return 0;
-}
-
-// } Driver Code Ends
