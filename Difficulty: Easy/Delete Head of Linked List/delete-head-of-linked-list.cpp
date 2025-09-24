8b3720f794c12b12c8e7702f141bc9edaee6566e
Sidharth <145455338+Sidharth70754@users.noreply.github.com>
2025-09-24 09:57:03 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Delete Head of Linked List/delete-head-of-linked-list.cpp
@@ -0,0 +1,26 @@
+/*
+class Node {
+public:
+    int data;
+    Node* next;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+*/
+class Solution {
+  public:
+    Node *deleteHead(Node *head) {
+        
+        if(head == NULL)  delete head;
+        
+        Node* temp = head;
+        head = head->next;
+        delete temp;
+        return head;
+        
+    }
+};
