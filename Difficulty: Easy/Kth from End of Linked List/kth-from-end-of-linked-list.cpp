05d1c580098f5afda095e67d4fa04b05cea65901
Sidharth <145455338+Sidharth70754@users.noreply.github.com>
2025-09-25 12:08:45 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Kth from End of Linked List/kth-from-end-of-linked-list.cpp
@@ -0,0 +1,31 @@
+/* struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};*/
+
+class Solution {
+public:
+    int getKthFromLast(Node* head, int k) {
+        if (head == NULL) return -1;
+
+        Node* slow = head;
+        Node* fast = head;
+
+        for(int i = 0; i < k; i++){
+            if(fast == NULL) return -1;
+            fast = fast->next;
+        }
+        
+        while(fast != NULL){
+            slow = slow->next;
+            fast = fast->next;
+        }
+
+        return slow->data; 
+        
+    }
+};
